08aaaf47afabcd17ead5008e6852c7d4928eb8b9
SoGoLocal <oleg.sokur1000@gmail.com>
2020-04-13 11:24:57 +0300

    Refactored

--- server/Core/server.js
+++ server/Core/server.js
@@ -7,12 +7,6 @@ const config = {
   port: 8000
 };
 
-//app.all('/*', function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//  next();
-//});
-
 app.use("/api/", cats);
 app.use(express.static('static'));
 
